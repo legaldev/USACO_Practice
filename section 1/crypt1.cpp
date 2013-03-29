07231726df6a4417979c0ea19e981637ec4c985e
lgl <luguiliang@gmail.com>
2013-03-29 20:00:02 +0800

    Prime Cryptarithm
    Write a program that will find all solutions to the cryptarithm above
    for any subset of digits from the set {1,2,3,4,5,6,7,8,9}

--- section 1/crypt1.cpp
+++ section 1/crypt1.cpp
@@ -7,8 +7,27 @@ LANG: C++
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+
+bool checknum(int num, const vector<int>& nums){
+	while(num > 0){
+		int final = num % 10;
+		int i;
+		for(i=0; i!=nums.size(); ++i){
+			if(final == nums[i]){
+				break;
+			}
+		}
+		if(i == nums.size())
+			return false;
+		num /= 10;
+	}
+	return true;
+}
+
 
 int main(){
 	ifstream file_in("crypt1.in");
@@ -18,8 +37,49 @@ int main(){
 		return 0;
 	}
 
-	
+	int n;
+	file_in >> n;
+	vector<int> nums(n);
+	for(int i=0; i!=n; ++i){
+		file_in >> nums[i];
+	}
 
+	sort(nums.begin(), nums.end());
+	for(int i=0; i!=nums.size(); ++i)
+		cout << nums[i] << endl;
+	// complete search
+	int count=0;
+	for(int i=0; i!=n; ++i){
+		for(int j=0; j!=n; ++j){
+			for(int k=0; k!=n; ++k){
+				int num1 = nums[i]*100 + nums[j]*10 + nums[k];
+
+				for(int s=0; s!=n; ++s){
+					int pro2 = (num1 * nums[s]);
+					if(pro2 > 999 || !checknum(pro2, nums))
+						continue;
+
+					for(int t=0; t!=n; ++t){
+						
+						int pro1 = (num1 * nums[t]);
+						if(pro1 > 999 || !checknum(pro1, nums))
+							continue;
+
+						int sum = (pro2 * 10 + pro1);
+						if(sum > 9999 || !checknum(sum, nums))
+							continue;
+						++count;
+						//cout << sum << endl;
+					}
+				}
+			}
+		}
+	}
+
+
+	cout << count << endl;
+
+	file_out << count << endl;
 	file_in.close();
 	file_out.close();
 	return 0;
