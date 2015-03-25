8c64e42ff2fd8ab6411048207bfece429b696b63
legaldev <legaldev0@gmail.com>
2015-03-25 11:58:26 +0800

    optimize prob Combination Lock

--- section 1/combo.cpp
+++ section 1/combo.cpp
@@ -16,6 +16,7 @@ LANG: C++
 #define PROB_NAME "combo"
 #define INPUT_FILE PROB_NAME".in"
 #define OUTPUT_FILE PROB_NAME".out"
+#define abs(a) (a) >= 0 ? (a) : -(a)
 using namespace std;
 bool load();
 void save(ostream& out);
@@ -23,9 +24,8 @@ bool save();
 void solve();
 
 int N;
-int f1, f2, f3;			// farmer
-int m1, m2, m3;			// master
-int c1[3], c2[3];		// c1 is farmer john, c2 is master
+int f[3];			// farmer
+int m[3];			// master
 int count = 0;
 
 int main(){
@@ -46,10 +46,10 @@ bool load()
 
 	file_in >> N;
 	for(int i=0; i<3; i++)
-		file_in >> c1[i];
+		file_in >> f[i];
 
 	for (int i=0; i<3; i++)
-		file_in >> c2[i];
+		file_in >> m[i];
 
 	return true;
 }
@@ -73,94 +73,32 @@ bool save()
 	return true;
 }
 
+int dis(int a, int b)
+{
+	int ab = abs(a-b);
+	if(ab < 5)
+		return 5-ab;
+	else if(ab > N - 5)
+		return 5-(N-ab);
+	return 0;
+}
+
 void solve()
 {
-	int low1[3], low2[3], high1[3], high2[3];
-
-	int addt[2][2] = {0};
-	for(int i=0; i<3; i++)
-	{
-		low1[i] = (c1[i] - 2);
-		high1[i] = (c1[i] + 2);
-		low2[i] = (c2[i] - 2);
-		high2[i] = (c2[i] + 2);
-
-		if(low1[i] < 0)
-		{
-			low1[i] += N;
-			high1[i] += N;
-		}
-		if(low2[i] < 0)
-		{
-			low2[i] += N;
-			high2[i] += N;
-		}
-
-		if(high1[i] > N && high2[i] < N)
-		{
-			low2[i] += N;
-			high2[i] += N;
-		}
-		if(high2[i] > N && high1[i] < N)
-		{
-			low1[i] += N;
-			high1[i] += N;
-		}
-	}
-
-	PVL(low1, 3)
-	PVL(high1, 3)
-	PVL(low2, 3)
-	PVL(high2, 3)
-
 	if(N <= 5)
 	{
-		count = N * N * N;
+		count = N*N*N;
 		return;
-	}	
-
-	// 检查两个码的重合部分
-	bool overlap[3] = {false};
-	int low[3], high[3];
-	for(int i=0;i<3;i++)
-	{
-		if(high1[i] >= low2[i] && high1[i] <= high2[i])
-		{
-			overlap[i] = true;
-			low[i] = low2[i];
-			high[i] = high1[i];
-		}
-		else if(high2[i] >= low1[i] && high2[i] <= high1[i])
-		{
-			overlap[i] = true;
-			low[i] = low1[i];
-			high[i] = high2[i];
-		}
 	}
 
-	bool realoverlap = true;
+	int minus = 1;
 	for(int i=0; i<3; i++)
-		realoverlap &= overlap[i];
-
-	count = 250;
-	PV(realoverlap)
-	if(realoverlap)
 	{
-		PVL(low, 3);
-		PVL(high, 3);
+		int d = dis(f[i], m[i]);
+		minus *= d;
 	}
 
-	if(realoverlap)
-	{
-		int minus = 1;
-		for(int i=0; i<3; ++i)
-		{
-			minus *= high[i] - low[i] + 1;
-		}
-		count -= minus;
-	}
+	PV(minus)
 
-	int max = N * N * N;
-	if(count > max)
-		count = max;
+	count = 250 - minus;
 }
