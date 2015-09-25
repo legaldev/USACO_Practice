ec0aca786a06c3991eca36d8fb72e63931127b5a
legaldev <legaldev0@gmail.com>
2015-09-25 20:12:17 +0800

    add kimbits desc

---
+++ section3/kimbits.cpp
@@ -0,0 +1,62 @@
+/*
+ID: luglian1
+PROG: kimbits
+LANG: C++
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#define PV(v) std::cout << #v << " = " << v << std::endl;
+#define PVL(v, n) \
+	std::cout << #v << " = ";\
+	for(int _i=0; _i<n; ++_i) \
+		{std::cout << v[_i] << ", ";}\
+	std::cout << std::endl;
+#define PROB_NAME "kimbits"
+#define INPUT_FILE PROB_NAME".in"
+#define OUTPUT_FILE PROB_NAME".out"
+
+using namespace std;
+
+bool load();
+void save(ostream& out);
+bool save();
+void solve();
+
+int main(){
+	load();
+	save();
+	return 0;
+}
+
+bool load()
+{
+	ifstream file_in(INPUT_FILE);
+	if(!file_in)
+	{
+		cerr << "Error: Failed to load file." << endl;
+		return false;
+	}
+
+	return true;
+}
+
+void save(ostream& out)
+{
+
+}
+
+bool save()
+{
+	ofstream file_out(OUTPUT_FILE);
+	if(!file_out){
+		cerr << "Error: Failed to open file." << endl;
+		return false;
+	}
+
+	save(file_out);
+	save(cout);
+
+	return true;
+}
