7b2efc0e7726a606024f16a1c3c7f862e1508bb5
legaldev <legaldev0@gmail.com>
2015-10-24 17:49:38 +0800

    modify camelot

--- section3/camelot.cpp
+++ section3/camelot.cpp
@@ -8,6 +8,7 @@ LANG: C++
 #include <string>
 #include <fstream>
 #include <vector>
+#include <queue>
 #define PV(v) std::cout << #v << " = " << v << std::endl;
 #define PVL(v, n) \
 	std::cout << #v << " = ";\
@@ -26,7 +27,7 @@ using namespace std;
 
 int row = 0, col = 0;
 int maxs = 0;
-
+int res = 0;
 int grid[MAX_SIZE][MAX_SIZE] = {0};
 
 int king = 0;
@@ -36,9 +37,12 @@ bool load();
 void save(ostream& out);
 bool save();
 void solve();
+void solve2();
 
 int main(){
 	load();
+	solve2();
+
 	save();
 	return 0;
 }
@@ -47,6 +51,12 @@ int main(){
 int getPos(int r, const string& c)
 {
 	return (r-1) * col + c[0] - 'A';
+}
+
+void getrc(int pos, int& r, int& c)
+{
+	r = pos / col;
+	c = pos - r * col;
 }
 
 bool load()
@@ -60,7 +70,7 @@ bool load()
 
 	file_in >> row >> col;
 
-	maxsq = row * col;
+	maxs = row * col;
 
 	int r;
 	string c;
@@ -70,20 +80,24 @@ bool load()
 
 	while(file_in.eof() == 0)
 	{
-		file_in >> c >> r;
+		file_in >> c;
+		if(file_in.eof())
+			break;
+		file_in >> r;
 		knight.push_back(getPos(r, c));
 	}
 
 	PV(row)
 	PV(col)
 	PV(king)
+	PV(knight.size())
 	PVL(knight, knight.size())
 	return true;
 }
 
 void save(ostream& out)
 {
-
+	out << res << endl;
 }
 
 bool save()
@@ -107,9 +121,9 @@ int vn[MAX_SIZE];
 int mark[MAX_SIZE];
 int vton[MAX_SIZE];
 
-inline compare(int src, int i, int j)
+inline bool compare(int src, int i, int j)
 {
-	dist[src][i] > dist[src][j];
+	return grid[src][i] > grid[src][j];
 }
 
 vector<int> find_near(int c)
@@ -120,7 +134,7 @@ vector<int> find_near(int c)
 		int n = c + neardiff[i];
 		if(n < 0 || n >= maxs)
 			continue;
-		res.push_back(maxs);
+		res.push_back(n);
 	}
 	return res;
 }
@@ -138,13 +152,17 @@ void goup_heap(int src, int i)
 	{
 
 		p = (i - 1) / 2;
+
 		if(p >=0 && compare(src, vn[p], vn[i]))
 		{
 			my_swap(p, i);
 			i = p;
 		}
 		else
+		{
 			break;
+		}
+
 	}while(i > 0);
 
 }
@@ -187,10 +205,10 @@ void godown_heap(int src, int i, int n)
 	}while(p < n);
 }
 
-void my_pop_heap(int n)
+void my_pop_heap(int src, int n)
 {
 	my_swap(0, n-1);
-	godown_heap(0, n-1);
+	godown_heap(src, 0, n-1);
 }
 
 void dijkstra(int src)
@@ -203,34 +221,36 @@ void dijkstra(int src)
 	{
 		vton[vn[i]] = i;
 	}
-
-
 	vector<int> ns = find_near(src);
+
+	grid[src][src] = 0;
+	goup_heap(src, src);
+
 	for(vector<int>::iterator it=ns.begin(); it!=ns.end(); ++it)
 	{
 		grid[src][*it] = 1;
 		goup_heap(src, *it);
-	}
 
-	//make_heap(vn, vn+P, c);
+	}
 
 	for(int i=maxs; i>0; --i)
 	{
-		//int n = vn[0];
 
-		my_pop_heap(i);
-		// pop_heap(vn, vn+i, c);
+		int n = vn[0];
+
+		my_pop_heap(src, i);
 
 		for(int j=0; j<8; ++j)
 		{
-			int next = c + neardiff[i];
+			int next = n + neardiff[j];
 			if(next < 0 || next >= maxs)
 				continue;
 			
-			if(vton[next] <= i-1 && grid[src][n] + grid[n][next] < grid[src][next])
+
+			if(vton[next] <= i-1 && grid[src][n] + 1 < grid[src][next])
 			{
-				grid[src][next] = grid[src][n] + grid[n][next];
-				goup_heap(vton[next], i-1);
+				grid[src][next] = grid[src][n] + 1;
+				goup_heap(src, vton[next]);
 			}
 		}
 	}
@@ -245,11 +265,175 @@ void solve()
 
 	for(int i=0; i<8; ++i)
 	{
-		neardiff[i] = near[i][0] * row + newr[i][1];
+		neardiff[i] = near[i][0] * row + near[i][1];
+	}
+	PVL(neardiff, 8)
+
+	for(int i=0; i<knight.size(); ++i)
+	{
+		dijkstra(knight[i]);
 	}
 
-	
+	for(int i=0; i<knight.size(); ++i)
+	{
+		PVL(grid[knight[i]], maxs)
+	}
 
 }
 
 
+void bfs(int src)
+{
+	queue<int> node;
+	node.push(src);
+	grid[src][src] = 0;
+
+	while(!node.empty())
+	{
+		int cur = node.front();
+		int r, c;
+		getrc(cur, r, c);
+		node.pop();
+		int move = grid[src][cur];
+		for(int j=0; j<8; ++j)
+		{
+			int next = cur + neardiff[j];
+			if(r + near[j][0] < 0 || r + near[j][0] >= row || c + near[j][1] < 0 || 
+				c + near[j][1] >=col || next < 0 || next >= maxs || grid[src][next] < INF)
+				continue;
+			grid[src][next] = move + 1;
+			node.push(next);
+		}
+	}
+}
+
+int near_knight[MAX_SIZE];
+int king_grid[MAX_SIZE][MAX_SIZE] = {0};
+int res_grid[MAX_SIZE] = {0};
+
+int king_dist(int src, int dst)
+{
+	int sr, sc;
+	getrc(src, sr, sc);
+	int dr, dc;
+	getrc(dst, dr, dc);
+
+	int r = abs(dr - sr);
+	int c = abs(dc - sc);
+	return r + c - min(r, c);
+}
+
+void king_path()
+{
+	for(int i=0; i<maxs; ++i)
+		for(int j=0; j<maxs; ++j)
+			king_grid[i][j] = king_dist(i, j);
+}
+
+void solve2()
+{
+	for(int i=0; i<maxs; ++i)
+		for(int j=0; j<maxs; ++j)
+			{
+				grid[i][j] = INF;
+				king_grid[i][j] = INF;
+			}
+
+
+	for(int i=0; i<8; ++i)
+	{
+		neardiff[i] = near[i][0] * row + near[i][1];
+	}
+
+	// PVL(neardiff, 8)
+
+	for(int i=0; i<maxs; ++i)
+	{
+		bfs(i);
+	}
+
+	for(int i=0; i<maxs; ++i)
+	{
+		int neard = INF;
+		near_knight[i] = -1;
+		for(int j=0; j<knight.size(); ++j)
+		{
+			if(grid[knight[j]][i] < neard)
+			{
+				neard = grid[knight[j]][i];
+				near_knight[i] = knight[j];
+			}
+		}
+	}
+
+	// PVL(near_knight, maxs)
+
+	// for(int i=0; i<knight.size(); ++i)
+	// {
+	// 	PVL(grid[knight[i]], maxs)
+	// }
+
+	king_path();
+
+	// PVL(king_grid[0], maxs)
+
+	if(knight.empty())
+		return;
+
+	for(int i=0; i<maxs; ++i)
+	{
+		res_grid[i] = king_grid[king][i];
+		for(int j=0; j<knight.size(); ++j)
+		{
+			res_grid[i] += grid[knight[j]][i];
+		}
+	}
+
+	PVL(res_grid, maxs)
+
+	res = INF;
+	int minp = 0;
+	for(int i=0; i<maxs; ++i)
+	{
+		int mind = 0;
+
+		for(int j=0; j<maxs; ++j)
+		{
+			for(int k=0; k<knight.size(); ++k)
+			{
+				//int kn = near_knight[j];
+				int kn = knight[k];
+				int d = king_grid[king][j] + grid[kn][j] + grid[j][i] - king_grid[king][i] - grid[kn][i];
+
+				// if(i == 33 && j == 27)
+				// {
+				// 	PV(near_knight[j])
+				// 	PV(grid[kn][j])
+				// 	PV(d)
+				// }
+
+				if(d < mind)
+					mind = d;
+			}
+		}
+
+		if(mind < 0)
+			res_grid[i] = res_grid[i] + mind;
+
+		if(res_grid[i] < res)
+		{
+			res = res_grid[i];
+			minp = i;
+		}
+	}
+
+	int r, c;
+	getrc(minp, r, c);
+	PV(minp)
+	PV(r)
+	PV(c)
+
+	// bfs(knight[0]);
+	// PVL(grid[knight[0]], maxs)
+
+}
